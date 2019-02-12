dfa9d1826aefed3474ba5f79d340db6e7709b95c
James Turk <dev@jamesturk.net>
2019-02-12 16:18:09 -0500

    url cleanup

--- openprecincts_web/urls.py
+++ openprecincts_web/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),
-    path('', include('files.urls')),
+    path('collect/', include('core.urls')),
+    path('files/', include('files.urls')),
     path('accounts/', include('accounts.urls')),
 ]
