3327d50d435e31c0e8efd5ab1f6e8dc03c85869a
James Turk <dev@jamesturk.net>
2019-02-13 14:07:36 -0500

    add .travis

--- accounts/views.py
+++ accounts/views.py
@@ -3,8 +3,7 @@ from django.conf import settings
 from django.contrib.auth.models import User
 from django.core.mail import send_mail
 from django.db import transaction
-from django.shortcuts import render, redirect
-from django.views import View
+from django.shortcuts import render
 from django.views.generic.edit import FormView
 import sesame.utils
 from core.utils import all_states
