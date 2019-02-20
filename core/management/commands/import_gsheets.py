74bb6edbf7cc6657d4ef6d7cca3826b824392562
James Turk <dev@jamesturk.net>
2019-02-19 20:14:04 -0500

    import filelog to S3

--- core/management/commands/import_gsheets.py
+++ core/management/commands/import_gsheets.py
@@ -1,9 +1,12 @@
+import os
 import re
 import csv
+import glob
 from django.db import transaction
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 from core.models import Locality, Official
+from files.utils import upload_local_file, File
 
 FIX_MAPPING = {
     'Isle of Wight': 'Isle of Wight County',
@@ -11,6 +14,14 @@ FIX_MAPPING = {
     'Richmond': 'Richmond, City of',
     'Virginia Beach': 'Virginia Beach, City of',
 }
+
+
+def get_locality(state, locality_name):
+    locality_name = re.sub(' [cC]ity$', ', City of', locality_name)
+    locality_name = FIX_MAPPING.get(locality_name, locality_name)
+    # print(locality_name)
+    locality = Locality.objects.get(state_id=state, name=locality_name)
+    return locality
 
 
 @transaction.atomic
@@ -23,10 +34,7 @@ def import_contact_csv(state, filename):
         for _ in range(3):
             f.readline()
         for line in csv.DictReader(f):
-            locality_name = re.sub(' [cC]ity$', ', City of', line['Locality'])
-            locality_name = FIX_MAPPING.get(locality_name, locality_name)
-            print(locality_name)
-            locality = Locality.objects.get(state_id=state, name=locality_name)
+            locality = get_locality(state, line['Locality'])
             o = Official.objects.create(
                 locality=locality,
                 first_name=line['First Name'],
@@ -45,6 +53,20 @@ def import_contact_csv(state, filename):
                                              notes='sent mail merge message')
 
 
+@transaction.atomic
+def import_sourcefiles(state, path):
+    bot, _ = User.objects.get_or_create(username='migration-bot')
+
+    all_files = glob.glob(path)
+    for file in all_files:
+        dir, fname = os.path.split(file)
+        dir, county = os.path.split(dir)
+        print(county, fname)
+
+        locality = get_locality(state, county)
+        upload_local_file(file, locality=locality, stage='S', created_by=bot)
+
+
 class Command(BaseCommand):
     help = 'Import data from Google Drive'
 
@@ -52,4 +74,6 @@ class Command(BaseCommand):
         parser.add_argument('--contact', type=str, help='path to contact CSV')
 
     def handle(self, *args, **options):
-        import_contact_csv('VA', options['contact'])
+        File.objects.all().delete()
+        # import_contact_csv('VA', options['contact'])
+        import_sourcefiles('VA', '/Users/james/Downloads/Virginia p*/*/source*Q*')
--- files/utils.py
+++ files/utils.py
@@ -1,5 +1,7 @@
+import os
 import uuid
 import boto3
+import magic
 from django.conf import settings
 from django.db import transaction
 from .models import File
@@ -16,29 +18,66 @@ def get_from_s3(key):
     return s3.get_object(Bucket=bucket, Key=key.s3_path)['Body']
 
 
-def upload_django_file(file, *, stage, locality, created_by):
+def _upload_file(*,
+                 stage, locality, mime_type, size, source_filename, created_by,
+                 file_path=None,
+                 file_obj=None,
+                 ):
     new_uuid = uuid.uuid4()
-    s3_path = make_s3_path(locality, new_uuid, stage, file.name)
+    s3_path = make_s3_path(locality, new_uuid, stage, source_filename)
 
     # do the s3 upload
     s3 = boto3.client('s3')
     bucket = settings.RAW_FILE_S3_BUCKET
+
+    # must specify one or the other
+    assert file_path or file_obj and not (file_path and file_obj)
 
     with transaction.atomic():
         # write the record first so we don't ever lose track of a file
         File.objects.create(
             id=new_uuid,
             stage=stage,
-            mime_type=file.content_type,
-            size=file.size,
+            mime_type=mime_type,
+            size=size,
             s3_path=s3_path,
             locality=locality,
-            source_filename=file.name,
+            source_filename=source_filename,
             created_by=created_by,
         )
 
         # do the upload inside the transaction so we can roll back if needed
-        if hasattr(file, 'temporary_file_path'):
-            s3.upload_file(file.temporary_file_path(), bucket, s3_path)
-        else:
-            s3.upload_fileobj(file, bucket, s3_path)
+        if file_path:
+            s3.upload_file(file_path, bucket, s3_path)
+        elif file_obj:
+            s3.upload_fileobj(file_obj, bucket, s3_path)
+
+
+def upload_local_file(filename, *, stage, locality, created_by):
+    _upload_file(
+        stage=stage,
+        locality=locality,
+        mime_type=magic.from_file(filename, mime=True),
+        size=os.path.getsize(filename),
+        source_filename=os.path.basename(filename),
+        created_by=created_by,
+        file_path=filename,
+    )
+
+
+def upload_django_file(file, *, stage, locality, created_by):
+    kwarg = {}
+    if hasattr(file, 'temporary_file_path'):
+        kwarg = {'file_name': file.temporary_file_path()}
+    else:
+        kwarg = {'file_obj': file}
+
+    _upload_file(
+        stage=stage,
+        locality=locality,
+        mime_type=file.content_type,
+        size=file.size,
+        source_filename=file.name,
+        created_by=created_by,
+        **kwarg,
+    )
