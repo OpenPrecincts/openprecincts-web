cb789b778ee853e16aac4230964848fc757eb467
James Turk <dev@jamesturk.net>
2019-02-20 19:14:03 -0500

    add dynamic configuration, part of #17

--- openprecincts_web/settings.py
+++ openprecincts_web/settings.py
@@ -1,22 +1,44 @@
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
+if os.environ.get('DEBUG', 'true').lower() == 'false':
+    # production settings
+    DEBUG = False
+    ALLOWED_HOSTS = ['*']
+    SECRET_KEY = os.environ['SECRET_KEY']
+    # ADMINS list should be 'Name Email, Name Email, Name Email...'
+    ADMINS = [a.rsplit(' ', 1) for a in os.environ.get('ADMINS', '').split(',')]
+    # EMAIL_HOST = os.environ['EMAIL_HOST']
+    # EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+    # EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+    # EMAIL_PORT = '587'
+    # EMAIL_USE_TLS = True
+    REGISTRATION_DEFAULT_FROM_EMAIL = DEFAULT_FROM_EMAIL = SERVER_EMAIL = os.environ.get(
+        'DEFAULT_FROM_EMAIL', 'bounce@openprecincts.org')
+    RAW_FILE_S3_BUCKET = os.environ['RAW_FILE_S3_BUCKET']
+else:
+    # dev settings
+    DEBUG = True
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'debug-secret-key')
+    ALLOWED_HOSTS = ['*']
+    INTERNAL_IPS = ['127.0.0.1']
+    DOMAIN = 'http://localhost:8000'
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    RAW_FILE_S3_BUCKET = "openprecincts-dev"
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j^n5y)6+ronofmq&eb%q%u$=nykhk_n#x7pfol%gc5mv^&bc$4'
+DATABASE_URL = os.environ.get(
+    'DATABASE_URL',
+    'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+)
+DATABASES = {'default': dj_database_url.parse(DATABASE_URL)}
+CONN_MAX_AGE = 60
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
-
-
-# Application definition
+# non-dynamic configuration
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -67,21 +89,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'openprecincts_web.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
-# Password validation
-# https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -97,26 +104,18 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/2.1/topics/i18n/
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
 # third party apps
-
 WEBPACK_LOADER = {
     'DEFAULT': {
         'CACHE': not DEBUG,
@@ -128,10 +127,4 @@ WEBPACK_LOADER = {
     }
 }
 
-
 SESAME_MAX_AGE = 30*60      # 30 minutes
-
-# Custom Settings
-
-RAW_FILE_S3_BUCKET = "raw.openprecincts.org"
-RAW_FILE_S3_BUCKET = "pgp-jpt-test"
