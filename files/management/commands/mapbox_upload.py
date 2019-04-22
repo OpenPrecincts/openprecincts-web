238738492cc54d31718e46993ad082ec4afe44d3
James Turk <dev@jamesturk.net>
2019-04-22 17:25:08 -0400

    working prototype of mapbox_upload, #33

--- files/management/commands/mapbox_upload.py
+++ files/management/commands/mapbox_upload.py
@@ -1,5 +1,7 @@
 import time
 from django.core.management.base import BaseCommand
+from files.models import File
+from files.utils import get_from_s3
 from mapbox import Uploader
 
 
@@ -8,19 +10,17 @@ def upload_shapefile(data, dataset_name):
     upload_resp = service.upload(data, dataset_name)
     if upload_resp.status_code == 201:
         upload_id = upload_resp.json()['id']
-        print(upload_resp.json())
         while True:
             status = service.status(upload_id)
             if status.status_code != 200:
                 break
             print(status.status_code)
             status = status.json()
-            print(status)
             if status['complete']:
                 break
             else:
                 print(status)
-            time.sleep(30)
+            time.sleep(10)
     else:
         print('Upload failed with status', upload_resp.status_code)
 
@@ -28,6 +28,11 @@ def upload_shapefile(data, dataset_name):
 class Command(BaseCommand):
     help = "Upload data to Mapbox"
 
+    def add_arguments(self, parser):
+        parser.add_argument("state", help="State to upload mbtiles for.")
+
     def handle(self, *args, **options):
-        with open('us_counties.mbtiles', 'rb') as data:
-            upload_shapefile(data, "us-counties")
+        f = File.objects.get(cycle__state=options['state'].upper(),
+                             mime_type="application/vnd.mapbox-vector-tile")
+        data = get_from_s3(f)
+        upload_shapefile(data, f"{options['state']}-precincts")
