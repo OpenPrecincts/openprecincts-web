ce43d0e5c711b338c46e6e8e385c689e975c8b2c
James Turk <dev@jamesturk.net>
2019-05-17 14:03:51 -0400

    simplify state status tracking in db

--- core/models.py
+++ core/models.py
@@ -5,10 +5,9 @@ from markupfield.fields import MarkupField
 
 class StateStatus(Enum):
     UNKNOWN = "unknown"
-    # TODO: restore waiting status
-    # WAITING = 'waiting'
     COLLECTION = "collection"
     CLEANING = "cleaning"
+    PRIOR_YEAR = "prior-year"
     AVAILABLE = "available"
 
 
@@ -22,57 +21,12 @@ class State(models.Model):
     census_geoid = models.CharField(max_length=2, unique=True)
 
     status_text = MarkupField(markup_type="markdown", default="")
-
-    # settings
-    task_collect = models.BooleanField(null=True, default=None)
-    task_contact = models.BooleanField(null=True, default=None)
-    task_files = models.BooleanField(null=True, default=None)
-    task_digitization = models.BooleanField(null=True, default=None)
-    task_verification = models.BooleanField(null=True, default=None)
-    task_published = models.BooleanField(null=True, default=None)
-
-    def _calc_status(self, *tasks):
-        # all portions of this are undecided
-        if all([t is None for t in tasks]):
-            return "inactive"
-        # if it is active, we're either all done or wip
-        if any([t is False for t in tasks]):
-            return "wip"
-        else:
-            return "complete"
-
-    def collection_status(self):
-        return self._calc_status(self.task_collect, self.task_contact, self.task_files)
-
-    def cleaning_status(self):
-        if self.collection_status() == "wip":
-            return "inactive"
-        return self._calc_status(self.task_digitization)
-
-    def final_status(self):
-        if self.collection_status() == "wip" or self.cleaning_status() == "wip":
-            return "inactive"
-        return self._calc_status(self.task_verification, self.task_published)
+    status = models.CharField(max_length=10, default=StateStatus.UNKNOWN.value,
+                              choices=((c.value, c.value) for c in StateStatus)
+                              )
 
     def current_cycle(self):
         return self.cycles.order_by("-year")[0]
-
-    def status(self):
-        if (
-            self.collection_status() == "inactive"
-            and self.cleaning_status() == "inactive"
-            and self.final_status() == "inactive"
-        ):
-            return StateStatus.UNKNOWN
-        if self.collection_status() == "wip":
-            return StateStatus.COLLECTION
-        if self.final_status() == "complete":
-            return StateStatus.AVAILABLE
-        if self.collection_status() == "complete" and (
-            self.cleaning_status() == "wip" or self.final_status() == "wip"
-        ):
-            return StateStatus.CLEANING
-        return StateStatus.UNKNOWN
 
     def __str__(self):
         return self.name
