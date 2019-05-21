7b3b6a726fc1d260e34df0bd316811e5b63b329a
James Turk <dev@jamesturk.net>
2019-05-21 18:57:45 -0400

    docker-compose for rabbitmq and postgres

--- openprecincts_web/settings.py
+++ openprecincts_web/settings.py
@@ -38,7 +38,7 @@ else:
     RAW_FILE_S3_BUCKET = os.environ.get("RAW_FILE_S3_BUCKET", "openprecincts-dev")
 
 DATABASE_URL = os.environ.get(
-    "DATABASE_URL", "sqlite:///" + os.path.join(BASE_DIR, "db.sqlite3")
+    "DATABASE_URL", "postgres://openprecincts:testpassword@localhost:5432/openprecincts"
 )
 DATABASES = {"default": dj_database_url.parse(DATABASE_URL)}
 CONN_MAX_AGE = 60
