5f10d987ee78db2eaab465044c813fa3f99ecfc9
James Turk <dev@jamesturk.net>
2019-05-22 10:29:58 -0400

    DATABASE_URL for tests

--- openprecincts_web/test_settings.py
+++ openprecincts_web/test_settings.py
@@ -1,5 +1,8 @@
 from .settings import *  # noqa
 
+DATABASE_URL = "sqlite://openprecincts.db"
+DATABASES = {"default": dj_database_url.parse(DATABASE_URL)}
+
 # disable whitenoise so we don't have to collectstatic for tests
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 MIDDLEWARE_REMOVE = ["whitenoise.middleware.WhiteNoiseMiddleware"]
