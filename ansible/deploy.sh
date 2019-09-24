29ecfdafd21c8df55ef5f1c06ad06991127c30f8
James Turk <dev@jamesturk.net>
2019-09-24 13:39:45 -0400

    ansible-galaxy step

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -3,5 +3,6 @@ set -e
 export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
 export AWS_DEFAULT_PROFILE=pgp
 
+ansible-galaxy install -r requirements.yml
 ansible-playbook openprecincts.yml -i inventory/ -l openprecincts_$1
 
