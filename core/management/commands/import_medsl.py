7970262ec86ce5bd09c21daaa365a0d77a93495b
James Turk <dev@jamesturk.net>
2019-09-30 14:51:49 -0400

    add import_medsl to get unmatched precincts

---
+++ core/management/commands/import_medsl.py
@@ -0,0 +1,40 @@
+import csv
+import functools
+from django.core.management.base import BaseCommand
+from core.models import State, PrecinctNameMatch
+
+
+@functools.lru_cache
+def get_state_obj(name):
+    return State.objects.get(name=name)
+
+
+class Command(BaseCommand):
+    help = "Import MEDSL Election data"
+
+    def add_arguments(self, parser):
+        parser.add_argument("filename", help="filename")
+
+    def handle(self, *args, **options):
+        seen = set()
+        with open(options["filename"]) as f:
+            csvf = csv.DictReader(f)
+
+            for row in csvf:
+                key = (row["state"], row["precinct"])
+                if key in seen:
+                    continue
+                seen.add(key)
+
+                PrecinctNameMatch.objects.create(
+                    state=get_state_obj(row["state"]),
+                    election_precinct_name=row["precinct"]
+                )
+
+                # TODO: add election results too
+                # row["office"]
+                # row["party"]
+                # row["votes"]
+                # row["candidate"]
+                # row["district"]
+                # row["year"]
