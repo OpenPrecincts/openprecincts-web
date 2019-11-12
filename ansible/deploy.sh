bc9fd0ecebb2d7026b97b8ec2cef540f1fede1d8
Hope Johnson <hopecaneel@gmail.com>
2019-11-12 10:33:54 -0500

    rm aws profile specification

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
-export AWS_DEFAULT_PROFILE=pgp
+# export AWS_DEFAULT_PROFILE=pgp
 
 ansible-galaxy install -r requirements.yml
 ansible-playbook openprecincts.yml -i inventory/ -l openprecincts_$1
